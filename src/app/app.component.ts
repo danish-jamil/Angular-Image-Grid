951b8a9eb3290dd9f0b4ed2f006b2ebc06f2f338
Danish Jamil <mrdanishjameel@gmail.com>
2022-10-21 00:05:27 +0500

    setup initial layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule, MatGridListModule],
 })
 export class AppComponent {
   title = 'Angular-Image-Grid';
+
+  items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 }
